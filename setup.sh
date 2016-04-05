8d2cd155435564d6e9a02475a484a81ca8eeebd3
xgqfrms <xgqfrms@outlook.com>
2016-04-05 14:05:50 +0800

    ok

--- setup.sh
+++ setup.sh
@@ -4,11 +4,11 @@ sudo apt-get install -y mpg123
 #删除 rm
 # -r 递归删除参数中列出的所有目录，子目录
 # -f 忽略不存在的文件，不给提示
-sudo rm -rf /var/speak_raspi_ip/
+sudo rm -rf /var/raspberrypi/
 #移动 mv 
-sudo mv speak_raspi_ip /var/
+sudo mv raspberrypi /var/
 #管道 ？
-echo "/usr/bin/python /var/speak_raspi_ip/speak_ip.py &" | sudo tee -a /etc/rc.local
+echo "/usr/bin/python /var/raspberrypi/speak_ip.py &" | sudo tee -a /etc/rc.local
 
 echo "install finished!"
 #注释 #
