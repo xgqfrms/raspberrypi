c386729a2304f0f3dff06c0050ada38d3e4d4809
xgqfrms <xgqfrms@outlook.com>
2016-04-06 11:01:52 +0800

    apache

---
+++ LAMP/index.php
@@ -0,0 +1,17 @@
+<?php  
+  print <<< EOT  
+<!doctype html>  
+<html lang="en">  
+<head>  
+<meta charset="UTF-8">  
+<title>Test successful</title>  
+</head>  
+<body>  
+<h1>Test successful</h1>  
+<p>Congratulations.</p>  
+<p>Your webserver and PHP are working.</p>  
+</body>  
+</html>  
+EOT;  
+
+?> 
